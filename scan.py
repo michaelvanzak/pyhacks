30398c8d9bdb51b6b8a7e5ea5caa6adbd24e8a14
Michal Jarrin <michaljarrinzak@gmail.com>
2022-03-29 12:21:22 -0600

    first commit

---
+++ scan.py
@@ -0,0 +1,22 @@
+#import packages
+from lxml import etree
+from subprocess import Popen
+
+#import modules
+import argparse
+import os
+
+#functions definition
+def get_ip(machine_name):
+    pass
+
+#class definition
+class Scanner:
+    def __init__(self) -> None:
+        pass
+
+if __name__ == '__main__':
+    scan = Scanner()
+    print('hello')
+
+
