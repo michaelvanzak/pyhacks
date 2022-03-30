ff5fc95d5ae70c1c1bc94fc5f2dfa424b311507f
Michal Jarrin <michaljarrinzak@gmail.com>
2022-03-30 07:42:36 -0600

    a minor mod

--- tcp-server.py
+++ tcp-server.py
@@ -8,7 +8,7 @@ def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((IP, PORT))
     server.listen(5)
-    print(f'[*] Listening in {IP}:{PORT}')
+    print(f'[*] Listening on {IP}:{PORT}')
 
     while True:
         client, address = server.accept()
