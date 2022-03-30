1a64cce5defb6c9cec86458f01dfd3449ce057ba
Michal Jarrin <michaljarrinzak@gmail.com>
2022-03-30 07:56:45 -0600

    modded the ip + port to comm with server

--- tcp-simple-client.py
+++ tcp-simple-client.py
@@ -2,8 +2,8 @@
 import socket
 
 #define variables
-target_host = "www.google.com"
-target_port = 80
+target_host = "0.0.0.0"
+target_port = 9998
 
 #create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,7 +12,8 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
 
 #send some data
-client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+#client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send(b"ABCDEF")
 
 #receive some data
 response = client.recv(4096)
